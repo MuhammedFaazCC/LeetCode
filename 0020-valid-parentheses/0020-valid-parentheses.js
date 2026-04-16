6ec60b30272f0eb05140580d50308c7b1c5e79d3
MuhammedFaazCC <faazmuhammed73@gmail.com>
2026-04-17 02:10:25 +0530

    Time: 3 ms (70.22%), Space: 57.9 MB (14.01%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+    for(let char of s){
+        if(char === "(" || char === "{" || char === "["){
+            stack.push(char);
+        }else{
+            if(char === ")" && stack[stack.length -1] !== "("|| char === "}" && stack[stack.length -1] !== "{"|| char === "]" && stack[stack.length -1] !== "["){
+                return false;
+            }
+            stack.pop();
+        }
+    }
+    return stack.length === 0;
+};
