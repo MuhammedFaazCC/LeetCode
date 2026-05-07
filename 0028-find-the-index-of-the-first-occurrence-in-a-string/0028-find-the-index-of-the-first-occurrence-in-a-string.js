83cd6c188bd9937497fd1cfa723d0a9051e7eb3f
MuhammedFaazCC <faazmuhammed73@gmail.com>
2026-05-07 17:33:33 +0530

    Time: 0 ms (100%), Space: 52.4 MB (98.37%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if(!haystack.includes(needle)){
+        return -1
+    }
+    let str = "";
+    return haystack.indexOf(needle)
+};
